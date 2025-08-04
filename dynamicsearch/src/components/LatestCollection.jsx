d3262d9a44974164c7ad935a83fd3e25e14e6e26
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-08-04 16:53:08 -0300

    feat: lastest collection component

---
+++ dynamicsearch/src/components/LatestCollection.jsx
@@ -0,0 +1,35 @@
+import React, { use, useContext, useState, useEffect } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const LatestCollection = () => {
+  
+  const { products } = useContext(ShopContext);
+
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0,10));
+  },[])
+  
+  return (
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title title1={'LATEST'} title2={'COLLECTIONS'}/>
+        <p className='2-3/4 m-auto text-sm sm:text-sm md:text-base text-gray-600'>
+          Discover our latest collections. Explore a curated selection of products that redefine style and quality.
+        </p>
+      </div>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          latestProducts.map((product, index) => (
+            <ProductItem key={index} id={product._id} image={product.image} name={product.name} price={product.price} />
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default LatestCollection
