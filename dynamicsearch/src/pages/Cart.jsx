048fbfdcc703bf5bf3490debeb32f26c875d08aa
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-08-04 16:55:10 -0300

    feat: cart page

---
+++ dynamicsearch/src/pages/Cart.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import Title from '../components/Title';
+
+const Cart = () => {
+
+  const { products, corrency, shoppingBag } = useContext(ShopContext);
+  const [shoppingBagData, setShoppingBagData] = useState([]);
+
+  useEffect(() => {
+    const tempShoppingBagData = [];
+    for (const productId in shoppingBag) {
+      for (const size in shoppingBag[productId]) {
+        if (shoppingBag[productId][size] > 0) {
+          tempShoppingBagData.push({
+            _id: productId,
+            size: size,
+            quantity: shoppingBag[productId][size],
+          });
+        }
+      }
+    }
+    console.log(tempShoppingBagData);
+    setShoppingBagData(tempShoppingBagData);
+  }, [shoppingBag])
+
+  return (
+    <div className='border-t pt-14'>
+      <div className='text-2xl mb-3'>
+        <Title title1={'Your'} title2={'Shopping Bag'} />
+      </div>
+      <div className=''>
+        {
+          shoppingBagData.length > 0 ? (
+            shoppingBagData.map((item, index) => {
+              const producData = products.find(product => product._id === item._id)
+
+              return (
+                <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4' >
+                  <div className='flex items-start gap-6'>
+                    <img src={producData.image[0]} className='w-16 sm:w-20' />
+                    <div>
+                      <p className='text-xs sm:text-lg font-medium'>{producData.name}</p>
+                      <div className='flex items-center gap-5 mt-2'>
+                        <p className='px-2 sm:px-3 sm:py-1 rounded-[3px_2px] shadow-gray-300 shadow-[2px_4px]'>{corrency} {producData.price}</p>
+                        <p className='px-2 sm:px-3 sm:py-1 rounded-[3px_2px] shadow-gray-300 shadow-[2px_4px]'>Size: {item.size}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <input type='number' />
+                </div>
+              )
+
+            })
+          ) : (
+            <p className='text-gray-500'>Your shopping bag is empty.</p>
+          )
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Cart
