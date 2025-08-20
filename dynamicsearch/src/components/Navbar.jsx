f6515ff0334040dff1b1fd533189b4a85fda1744
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-08-20 11:37:14 -0300

    feat: link for page login and page orders

--- dynamicsearch/src/components/Navbar.jsx
+++ dynamicsearch/src/components/Navbar.jsx
@@ -32,11 +32,13 @@ const Navbar = () => {
         <div className='flex items-center gap-6'>
             <img onClick={()=> setShowSearch(!showSearch)} src={assets.search_icon} className='w-5 cursor-pointer' alt='Search' />
             <div className='group relative'>
-                <img src={assets.profile_icon} className='w-5 cursor-pointer' alt='Cart' />
+                <Link to={'/login'}>
+                    <img src={assets.profile_icon} className='w-5 cursor-pointer' alt='Cart' />
+                </Link>
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-                        <p className="cursor-pointer hover:text-black">My Profile</p>
-                        <p className="cursor-pointer hover:text-black">Orders</p>
+                        <Link><p className="cursor-pointer hover:text-black">My Profile</p></Link>
+                        <Link to={'/orders'}><p className="cursor-pointer hover:text-black">Orders</p></Link>
                         <p className="cursor-pointer hover:text-black">Logout</p>
                     </div>
                 </div>
